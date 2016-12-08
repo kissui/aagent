395a047f50cc7bad92b366b762e1ed19eab2cec8
cunde <a18811150085@163.com>
2016-12-08 11:45:54 +0800

    header

--- public/views/layout/header.jsx
+++ public/views/layout/header.jsx
@@ -71,7 +71,7 @@ module.exports = React.createClass({
 							</ul>
 						</div>
 						<div className="col-md-6 text-right">
-							dddd
+							{Auth.getUser()}
 						</div>
 					</div>
 				</div>
